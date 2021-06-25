d13359a18249b8ccb4e60f0d1e344c6181fc007d
danielchang2001 <danielchang083@gmail.com>
2021-06-25 00:06:17 -0700

    Changed name of server.js to index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require("cors")
 const spotifyWebApi = require('spotify-web-api-node')
 const lyricsFinder = require("lyrics-finder")
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 const path = require('path')
 
 const app = express();
