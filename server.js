877673153369c44bc80efed8d30b67bda05fb98a
danielchang2001 <danielchang083@gmail.com>
2021-06-24 23:41:16 -0700

    Fixing error#

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
+app.use(express.static('dist'));
+app.get('*', (request, response) => {
+response.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 
 app.post('/refresh', (req, res) => {
